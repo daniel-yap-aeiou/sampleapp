3bdac93d6e24d29cb6f0ca38330f3d02b70c67dc
Daniel Yap <danielytf@gmail.com>
2020-07-22 18:55:49 +1000

    Updates

---
+++ precache-manifest.b5de922304c027eb2bc675e30df1f2df.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d4a3590dece88ca6196874f6833abf",
+    "url": "/sampleapp/index.html"
+  },
+  {
+    "revision": "f4e4a1fda82854e76b71",
+    "url": "/sampleapp/static/css/2.4c093531.chunk.css"
+  },
+  {
+    "revision": "95aaf3df5385fb550b8c",
+    "url": "/sampleapp/static/css/main.63e8db95.chunk.css"
+  },
+  {
+    "revision": "f4e4a1fda82854e76b71",
+    "url": "/sampleapp/static/js/2.32720339.chunk.js"
+  },
+  {
+    "revision": "23692ada4101600d3769adeb08781fb7",
+    "url": "/sampleapp/static/js/2.32720339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95aaf3df5385fb550b8c",
+    "url": "/sampleapp/static/js/main.6f117f65.chunk.js"
+  },
+  {
+    "revision": "277d2ba992b3a71ea751",
+    "url": "/sampleapp/static/js/runtime-main.345fd405.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/sampleapp/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/sampleapp/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/sampleapp/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/sampleapp/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/sampleapp/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sampleapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
